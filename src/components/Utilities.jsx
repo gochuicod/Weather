df45a423770288af1f03cb86e7f4ee03ad7037f1
gochuicod <gochuicodarelleq@gmail.com>
2023-04-23 18:08:51 +0800

    React: Weather App v1.3.1

--- src/components/Utilities.jsx
+++ src/components/Utilities.jsx
@@ -3,10 +3,7 @@ import { Text } from "@mantine/core"
 const Utilities = {
     apiKey: "925f99c7a911db368626814750c8bc61",
     handleAtmosphericPressure: value => value < 1013 ? "Low" : "High",
-    handleLocalTime: value => {
-        const time = new Date(value*1000).toLocaleTimeString()
-        return time.substring(0,time.length-2)
-    },
+    handleLocalTime: value =>  new Date(value*1000).toLocaleTimeString(),
     handleAirQualityIndexLevel: (value,l1,l2,l3,l4) => {
         if(value > -1 && value < l1+1) return <Text fw={'bold'} c={'green'}>Good</Text>
         if(value > l1-1 && value < l2+1) return <Text fw={'bold'} c={'yellow'}>Fair</Text>
